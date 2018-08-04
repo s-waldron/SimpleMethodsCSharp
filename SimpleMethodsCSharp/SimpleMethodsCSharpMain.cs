438cbad3374f2238508175246426f838628c61fe
Stephen Waldron <30762163+s-waldron@users.noreply.github.com>
2018-08-04 14:05:18 -0400

    Orginal File Push

---
+++ SimpleMethodsCSharp/SimpleMethodsCSharpMain.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMethodsCSharp
+{
+    class SimpleMethodsCSharpMain
+    {
+        public static void Main(String[] args)
+        {
+            SimpleMethodsCSharpTest testMethods = new SimpleMethodsCSharpTest();
+            int[] testValues = { 1, 1, 1, 3, 7, 11, 7 };
+            int[] testValues1 = { 1, 3, 3, 5, 7 };
+            int[] testValues2 = { -1, 0, 1 };
+            int[] testValues3 = { -2, -2, -1, 0, 1, 0, -1 };
+            String testValues4 = "Hello World";
+            String testValues5 = "mom";
+            String testValues6 = "redivider";
+            List<int> test = testMethods.NonDupNumbers(testValues);
+            List<int> test1 = testMethods.NonDupNumbers(testValues1);
+            List<int> test2 = testMethods.NonDupNumbers(testValues2);
+            List<int> test3 = testMethods.NonDupNumbers(testValues3);
+            List<char> test4 = testMethods.NonDupLetters(testValues4);
+            int test5 = testMethods.TotalASCIIValueOfAString(testValues4);
+            bool test6 = testMethods.IsPalindrome(testValues4);
+            bool test7 = testMethods.IsPalindrome(testValues5);
+            bool test8 = testMethods.IsPalindrome(testValues6);
+            Console.Write("For the integer array: ");
+            foreach(int num in testValues)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.Write("The output from the method to find all non duplicated numbers is: ");
+            foreach(int num in test)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.Write("For the integer array: ");
+            foreach (int num in testValues1)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.Write("The output from the method to find all non duplicated numbers is: ");
+            foreach (int num in test1)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.Write("For the integer array: ");
+            foreach (int num in testValues2)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.Write("The output from the method to find all non duplicated numbers is: ");
+            foreach (int num in test2)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.Write("For the integer array: ");
+            foreach (int num in testValues3)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.Write("The output from the method to find all non duplicated numbers is: ");
+            foreach (int num in test3)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("For the string: " + testValues4);
+            Console.Write("The output from the method to find all non duplicated letter is: ");
+            foreach(char let in test4)
+            {
+                Console.Write(let + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("For the string: " + testValues4);
+            Console.Write("The output from the method to find the total ASCII value of the string is: ");
+            Console.WriteLine(test5);
+            Console.WriteLine("Is " + testValues4 + " a palindrome? " + test6);
+            Console.WriteLine("Is " + testValues5 + " a palindrome? " + test7);
+            Console.WriteLine("Is " + testValues6 + " a palindrome? " + test8);
+            Console.WriteLine("Press the enter key to exit!!!");
+            Console.Read();
+
+        }
+    }
+}
---
+++ SimpleMethodsCSharp/SimpleMethodsCSharpTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMethodsCSharp
+{
+    class SimpleMethodsCSharpTest
+    {
+        public List<int> NonDupNumbers (int[] values)
+        {
+            List<int> returnValues = new List<int>();
+            Boolean dups = false;
+            for(int outter = 0; outter < values.Length; outter++)
+            {
+                dups = false;
+                for(int inner = 0; inner < values.Length; inner++)
+                {
+                    if(inner != outter)
+                    {
+                        if(values[outter] == values[inner])
+                        {
+                            //Console.Write("Inside if(values[outter] == values[inner]) check");
+                            dups = true;
+                            //Console.Write(dups);
+                        }// end if(values[outter] == values[inner]) check
+                    }// end if(inner != outter) check
+                }// end inner for loop
+                if (dups == false)
+                {
+                    returnValues.Add(values[outter]);
+                }// end if(!dups) check
+            }// end outter for loop
+            return returnValues;
+        }//end NonDupNumbers method
+
+        public List<char> NonDupLetters(String inputString)
+        {
+            List<char> returnValues = new List<char>();
+            bool dups = false;
+            char[] orginalStringArray = inputString.ToCharArray();
+            for(int outter = 0; outter < inputString.Length; outter++)
+            {
+                dups = false;
+                for(int inner = 0; inner < inputString.Length; inner++)
+                {
+                    if(inner != outter)
+                    {
+                        if(orginalStringArray[outter] == orginalStringArray[inner])
+                        {
+                            dups = true;
+                        }// end if(orginalStringArray[outter] == orginalStringArray[inner]) check
+                    }// end if(inner != outter) check
+                }// end inner for loop
+                if (!dups)
+                {
+                    returnValues.Add(orginalStringArray[outter]);
+                }// end if (!dups) check
+            }// end outter for loop
+            return returnValues;
+        }// end NonDupLetters method
+
+        public int TotalASCIIValueOfAString (String inputString)
+        {
+            int total = 0;
+            char[] valueString = inputString.ToCharArray();
+            foreach(char let in valueString)
+            {
+                total += (int)let;
+            }// end char let foreach loop
+            return total;
+        }// end TotalASCIIValueOfAString method
+
+        public bool IsPalindrome (String inputString)
+        {
+            String reverseString = "";
+            for(int index = (inputString.Length - 1); index > -1; index--)
+            {
+                reverseString += inputString.Substring(index, 1);
+            }// end index for loop
+            if (inputString.Equals(reverseString))
+            {
+                return true;
+            }// end if (inputString.Equals(reverseString)) check
+            else
+            {
+                return false;
+            }// end else check
+        }// end IsPalindrome method
+    }// end SimpleMethodsCSharpTest class
+}// end SimpleMethodsCSharp namespace
